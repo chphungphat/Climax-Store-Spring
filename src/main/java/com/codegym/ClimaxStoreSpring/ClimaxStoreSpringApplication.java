9d3e37d77d8af147cb16afa916ffd213b7c7a323
chphungphat <chphungphat@gmail.com>
2023-07-06 09:23:37 +0700

    project initial

---
+++ src/main/java/com/codegym/ClimaxStoreSpring/ClimaxStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.ClimaxStoreSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaxStoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaxStoreSpringApplication.class, args);
+	}
+
+}
